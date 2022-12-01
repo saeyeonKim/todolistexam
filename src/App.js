dd463a1c172a63a812881a535970d8e10a0c1d31
saeyeonKim <118176015+saeyeonKim@users.noreply.github.com>
2022-12-01 11:42:51 +0900

    시험

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [contents, setContents] = useState([
+    {
+      id: 1,
+      todo: "react를 배워봅시다.",
+      isDone: true,
+    },
+    {
+      id: 2,
+      todo: "useState를 배워봅시다.",
+      isDone: true,
+    },
+  ]);
+
+  const [todo, setTodo] = useState("");
+
+  const addUserHandler = () => {
+    const newContent = {
+      id: contents.length + 1,
+      todo: todo,
+      isDone: true,
+    };
+    setContents([...contents, newContent]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input value={todo} onChange={(e) => setTodo(e.target.value)}></input>
+      <button onClick={addUserHandler}>추가하기</button>
+      <h1>Todo List</h1>
+      <div className="mycard">{todo}</div>
     </div>
   );
 }
